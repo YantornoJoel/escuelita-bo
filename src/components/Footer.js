14d8359e102797c5379a027b7d369cc957659af6
Joel <jyantorno@indec.gob.ar>
2022-06-22 00:22:34 -0300

    v1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,32 @@
+import { Logout } from '@mui/icons-material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../escuelita.png'
 
 export const Footer = () => {
+
+    const onLogout = () => {
+        localStorage.removeItem('token')
+        window.location.reload();
+    }
+
     return (
         <div className="footer" style={{ textAlign: 'center', marginBottom: '25px' }}>
             <img src={logo} height={150} width={300} alt="cavc" />
+
+            {
+                window.screen.width < 400 && localStorage.getItem('token') &&
+                (
+                    <li className="nav-item navbar-brand mt-4" >
+                        <Link
+                            className="nav-link"
+                            id='btn-login'
+                            to="/"
+                            onClick={() => onLogout()}>Cerrar sesión <Logout style={{ color: '#6f42c1' }} />
+                        </Link>
+                    </li>
+                )
+            }
         </div>
     )
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,39 @@
+import { Logout } from "@mui/icons-material";
 import React from "react";
 import { Link } from "react-router-dom";
 import { removeLocalStorage } from "./useForm";
 
 
 export const Navigation = () => {
+
+  const onLogout = () => {
+    localStorage.removeItem('token')
+    window.location.reload();
+  }
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
         <Link className="navbar-brand navegacion" to="/" onClick={() => removeLocalStorage()}>
           EscuelitaApp
         </Link>
-        <div className="navbar-nav ml-autos">
+        <div className="navbar-nav ml-auto" >
           <li className="nav-item navbar-brand ">
             <Link className="nav-link" to="/" onClick={() => removeLocalStorage()}>Listado</Link>
           </li>
-          <li className="nav-item navbar-brand ">
+          <li className="nav-item navbar-brand">
             <Link className="nav-link" to="/create">Crear</Link>
           </li>
+          {
+            window.screen.width > 400 && localStorage.getItem('token') &&
+            (
+              <li className="nav-item navbar-brand ">
+                <Link className="nav-link" id='btn-login' to="/" onClick={() => onLogout()}>Cerrar sesión < Logout /></Link>
+              </li>
+            )
+          }
         </div>
       </div>
-    </nav>
+    </nav >
   );
 }
